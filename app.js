f74040b0e9cca8b4b66bb5e6a987bea4f71d2712
Vineet <sharma24vineet@gmail.com>
2021-01-28 08:52:31 +0530

    before heroku completed

--- app.js
+++ app.js
@@ -43,6 +43,17 @@ app.post("/", function (req, res) {
         auth: "vineet:9ad0c784495f6e0d80b393f58d85a4d4-us7"
     }
     const request = https.request(url, options, function (response) {
+        if (response.statusCode === 200) {
+            res.sendFile(__dirname + "/success.html");
+
+
+        }
+        else {
+            res.sendFile(__dirname + "/failure.html");
+        }
+
+
+
         response.on("data", function (data) {
             console.log(JSON.parse(data));
         })
@@ -53,6 +64,10 @@ app.post("/", function (req, res) {
 
 
 });
+
+app.post("/failure", function (req, res) {
+    res.redirect("/")
+})
 
 
 app.listen(3000, function () {
